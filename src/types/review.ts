0312bf9e1ef95f2a2547a3857906a5050512ba7e
Jeonghoan Hoang <zldwkd93@gmail.com>
2023-09-10 19:27:39 +0200

    cleaned up

--- src/types/review.ts
+++
@@ -1,9 +0,0 @@
-import { Types } from "mongoose";
-
-export interface Review {
-  _id: Types.ObjectId;
-  userId: Types.ObjectId;
-  eventId?: Types.ObjectId | null;
-  rating: number;
-  text: string;
-}
