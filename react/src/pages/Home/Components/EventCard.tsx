4f72a58a609f79132e710a43f0b30365914e118c
Jeonghoan Hoang <zldwkd93@gmail.com>
2023-09-13 16:33:53 +0200

    implementng cards

--- react/src/pages/Home/Components/EventCard.tsx
+++ react/src/pages/Home/Components/EventCard.tsx
@@ -17,18 +17,20 @@ const EventCard: React.FC<EventCardProps> = ({
   userName,
   userPhoto,
 }) => (
-  <div className="border p-4 w-64 rounded-md">
-    <img
-      src={img}
-      alt={title}
-      className="w-full h-40 object-cover rounded-md"
-    />
-    <h2 className="text-xl font-bold my-2">{title}</h2>
-    <p className="text-gray-600">{desc}</p>
-    <p className="font-medium my-2">Price: ${price}</p>
-    <div className="flex items-center mt-4">
-      <img src={userPhoto} alt={userName} className="w-10 h-10 rounded-full" />
-      <span className="ml-4 text-lg">{userName}</span>
+  <div className="border w-full rounded-md m-3">
+    <img src={img} alt={title} className="w-full h-40 object-cover" />
+    <div className="p-4">
+      <h2 className="text-xl font-bold my-2">{title}</h2>
+      <p className="text-gray-600">{desc}</p>
+      <p className="font-medium my-2">Price: ${price}</p>
+      <div className="flex items-center mt-4">
+        <img
+          src={userPhoto}
+          alt={userName}
+          className="w-10 h-10 rounded-full"
+        />
+        <span className="ml-4 text-lg">{userName}</span>
+      </div>
     </div>
   </div>
 );
--- react/src/pages/Home/Components/EventSlider.tsx
+++ react/src/pages/Home/Components/EventSlider.tsx
@@ -1,5 +1,4 @@
-import React from "react";
-import ReactSlider from "react-slider";
+import React, { useState } from "react";
 import EventCard from "./EventCard";
 
 type Event = {
@@ -16,19 +15,48 @@ type EventSliderProps = {
 };
 
 const EventSlider: React.FC<EventSliderProps> = ({ events }) => {
+  const [startIndex, setStartIndex] = useState(0);
+
+  const handleNext = () => {
+    setStartIndex((prevIndex) => Math.min(prevIndex + 1, events.length - 1));
+  };
+
+  const handlePrev = () => {
+    setStartIndex((prevIndex) => Math.max(prevIndex - 1, 0));
+  };
+
   return (
-    <ReactSlider
-      value={0}
-      max={events.length - 1}
-      renderThumb={({ props }, state) => (
-        <div {...props}>
-          <EventCard {...events[state.value]} />
-        </div>
+    <div className="relative overflow-hidden h-auto">
+      <div
+        className="flex transition-transform duration-500"
+        style={{ transform: `translateX(-${startIndex * 100}%)` }}
+      >
+        {events.map((event) => (
+          <div
+            key={event.title}
+            className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 px-2"
+          >
+            <EventCard {...event} />
+          </div>
+        ))}
+      </div>
+      {startIndex > 0 && (
+        <button
+          className="absolute left-0 top-1/2 transform -translate-y-1/2 bg-gray-800 text-white px-3 py-2 rounded-full"
+          onClick={handlePrev}
+        >
+          ←
+        </button>
       )}
-      className="h-96"
-      thumbClassName="thumb"
-      trackClassName="track"
-    />
+      {startIndex < events.length - 1 && (
+        <button
+          className="absolute right-0 top-1/2 transform -translate-y-1/2 bg-gray-800 text-white px-3 py-2 rounded-full"
+          onClick={handleNext}
+        >
+          →
+        </button>
+      )}
+    </div>
   );
 };
 
