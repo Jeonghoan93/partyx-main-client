6200cb2c932187b0c7176f56d680cda1f07b6551
Jeonghoan Hoang <zldwkd93@gmail.com>
2023-09-14 12:42:34 +0200

    changed base URL conditinoal

--- react/src/services/api.ts
+++ react/src/services/api.ts
@@ -1,7 +1,12 @@
 import axios from "axios";
 
+const baseURL =
+  process.env.NODE_ENV === "production"
+    ? "https://yourbackendserver.com/api"
+    : "http://localhost:3001/api";
+
 const api = axios.create({
-  baseURL: "http://localhost:3001/api",
+  baseURL: baseURL,
   withCredentials: true,
 });
 
