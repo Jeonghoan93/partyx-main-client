a23e4e36e90b430ff1eca1c7e2dc11fdece68b5f
Jeonghoan Hoang <zldwkd93@gmail.com>
2023-09-26 19:48:34 +0200

    deleted all the modals excpet toaster and video

--- react/src/layouts/RootLayout.tsx
+++ react/src/layouts/RootLayout.tsx
@@ -1,22 +1,15 @@
 import { Outlet } from "react-router-dom";
+import FullPageModal from "src/components/FullPageModal";
 import Footer from "src/components/footer";
-import CreateProfileModal from "src/components/modals/CreateProfileModal";
-import FinishRegister from "src/components/modals/FinishRegisterModal";
-import LoginModal from "src/components/modals/LoginModal";
-import RegisterModal from "src/components/modals/RegisterModal";
-import RentModal from "src/components/modals/RentModal";
-import SearchModal from "src/components/modals/SearchModal";
 import Navbar from "src/components/navbar/Navbar";
+import ToasterProvider from "src/providers/ToasterProvider";
 
 export default function RootLayout() {
   return (
     <>
-      <LoginModal />
-      <FinishRegister />
-      <CreateProfileModal />
-      <RegisterModal />
-      <SearchModal />
-      <RentModal />
+      <FullPageModal />
+      <ToasterProvider />
+
       <Navbar />
 
       <div className="pb-20 pt-28">
