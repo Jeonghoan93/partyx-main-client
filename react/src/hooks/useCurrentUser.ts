4a0349d2b462ef8d71f10988bb4e75c30acdbe6f
Jeonghoan Hoang <zldwkd93@gmail.com>
2023-09-26 15:54:04 +0200

    checkign cookie presence before calling current user

--- react/src/hooks/useCurrentUser.ts
+++ react/src/hooks/useCurrentUser.ts
@@ -5,16 +5,23 @@ import { getCurrentUser } from "src/services/auth";
 export function useCurrentUser() {
   const [currentUser, setCurrentUser] = useState<User | null>(null);
 
+  function hasCookie(name: string): boolean {
+    return document.cookie
+      .split(";")
+      .some((item) => item.trim().startsWith(name + "="));
+  }
+
   useEffect(() => {
     async function fetchCurrentUser() {
-      try {
-        const user = await getCurrentUser();
-        setCurrentUser(user ?? null);
-      } catch (err) {
-        console.error("Error fetching user:", err);
+      if (hasCookie("Authentication")) {
+        try {
+          const user = await getCurrentUser();
+          setCurrentUser(user ?? null);
+        } catch (err) {
+          console.error("Error fetching user:", err);
+        }
       }
     }
-
     fetchCurrentUser();
   }, []);
 
