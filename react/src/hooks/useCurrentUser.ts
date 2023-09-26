7898c69e3a9e2280f1caa693b53d1c45b146a058
Jeonghoan Hoang <zldwkd93@gmail.com>
2023-09-26 15:46:47 +0200

    fixed type err

--- react/src/hooks/useCurrentUser.ts
+++ react/src/hooks/useCurrentUser.ts
@@ -9,11 +9,12 @@ export function useCurrentUser() {
     async function fetchCurrentUser() {
       try {
         const user = await getCurrentUser();
-        setCurrentUser(user);
+        setCurrentUser(user ?? null);
       } catch (err) {
         console.error("Error fetching user:", err);
       }
     }
+
     fetchCurrentUser();
   }, []);
 
