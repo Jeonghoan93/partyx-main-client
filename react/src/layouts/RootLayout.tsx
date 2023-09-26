705d35eddfb48fe359d682a69c7fc9810e1c4887
Jeonghoan Hoang <zldwkd93@gmail.com>
2023-09-26 20:01:17 +0200

    added all except search modal

--- react/src/layouts/RootLayout.tsx
+++ react/src/layouts/RootLayout.tsx
@@ -1,6 +1,11 @@
 import { Outlet } from "react-router-dom";
 import FullPageModal from "src/components/FullPageModal";
 import Footer from "src/components/footer";
+import CreateProfileModal from "src/components/modals/CreateProfileModal";
+import FinishRegister from "src/components/modals/FinishRegisterModal";
+import LoginModal from "src/components/modals/LoginModal";
+import RegisterModal from "src/components/modals/RegisterModal";
+import RentModal from "src/components/modals/RentModal";
 import Navbar from "src/components/navbar/Navbar";
 import ToasterProvider from "src/providers/ToasterProvider";
 
@@ -9,6 +14,12 @@ export default function RootLayout() {
     <>
       <FullPageModal />
       <ToasterProvider />
+      <LoginModal />
+      <FinishRegister />
+      <CreateProfileModal />
+      <RegisterModal />
+
+      <RentModal />
 
       <Navbar />
 
