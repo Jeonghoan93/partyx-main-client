002e098ed4cfd1af90c6c34b7cb3e71623008cb5
Jeonghoan Hoang <zldwkd93@gmail.com>
2023-10-09 11:45:32 +0200

    style changes

--- react/src/pages/Account/Components/LinkBox.tsx
+++ react/src/pages/Account/Components/LinkBox.tsx
@@ -17,11 +17,13 @@ export const LinkBox: React.FC<LinkBoxProps> = ({
       style={{
         maxWidth: "400px",
       }}
-      className="cursor-pointer p-3 flex flex-col gap-2 rounded-lg shadow-md"
+      className="cursor-pointer py-3 flex flex-row items-center gap-6"
     >
-      <span className="mb-2">{icon}</span>
-      <span className="font-bold text-[12pt]">{title}</span>
-      <span className="text-[10pt] text-gray-700">{subtitle}</span>
+      <span className="">{icon}</span>
+      <div className="flex flex-col">
+        <span className="font-semibold text-[11pt]">{title}</span>
+        <span className="font-light text-[9pt]">{subtitle}</span>
+      </div>
     </section>
   );
 };
