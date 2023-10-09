323ca89eeb77146adffed397c6e0708c0a27f5ee
Jeonghoan Hoang <zldwkd93@gmail.com>
2023-10-09 14:26:52 +0200

    Add all

--- react/src/pages/Career/Pages/Positions/Sections/HeaderPositions.tsx
+++ react/src/pages/Career/Pages/Positions/Sections/HeaderPositions.tsx
@@ -1,9 +1,11 @@
+import { jobList } from "../Constants/jobList";
+
 const HeaderPositions = () => {
   return (
     <div className="bg-black text-gray-50 p-6 flex flex-col items-center">
       <div style={{ maxWidth: "400px" }} className="text-center">
         <span className="font-semibold text-[25pt]">
-          0 Jobs across all teams and all locations
+          {jobList.length} Jobs across all teams and all locations
         </span>
       </div>
     </div>
