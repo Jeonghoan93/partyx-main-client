60faa9e842b728980d043a4b068470c30d16e068
Jeonghoan Hoang <zldwkd93@gmail.com>
2023-10-12 22:57:41 +0200

    Add all

--- react/src/components/modals/SimpleModal.tsx
+++ react/src/components/modals/SimpleModal.tsx
@@ -1,11 +1,12 @@
 import { useCallback, useEffect, useRef, useState } from "react";
+import { IoMdClose } from "react-icons/io";
 import useOnClickOutside from "src/hooks/userOnClickOutside";
 
 interface SimpleModalProps {
-  title?: string;
   isOpen?: boolean;
   onClose: () => void;
   onSubmit: () => void;
+  title?: string;
   body?: React.ReactElement;
   footer?: React.ReactElement;
   actionLabel?: string;
@@ -15,15 +16,15 @@ interface SimpleModalProps {
 }
 
 const SimpleModal: React.FC<SimpleModalProps> = ({
-  title,
-  isOpen = false,
+  isOpen,
   onClose,
-  body,
   onSubmit,
-  disabled = false,
+  title,
+  body,
+  disabled,
+  secondaryAction,
 }) => {
   const [showModal, setShowModal] = useState(isOpen);
-
   const modalRef = useRef<HTMLDivElement>(null);
 
   const handleClose = useCallback(() => {
@@ -37,55 +38,125 @@ const SimpleModal: React.FC<SimpleModalProps> = ({
     }, 300);
   }, [onClose, disabled]);
 
-  useOnClickOutside([modalRef], handleClose);
-
   useEffect(() => {
     setShowModal(isOpen);
   }, [isOpen]);
+
+  useOnClickOutside([modalRef], handleClose);
 
   const handleSubmit = useCallback(() => {
     if (disabled) {
       return;
     }
+
     handleClose();
     onSubmit();
-  }, [disabled, handleClose, onSubmit]);
+  }, [onSubmit, disabled, handleClose]);
 
-  if (!showModal) {
+  if (!isOpen) {
     return null;
   }
 
   return (
-    <div
-      className={
-        "fixed inset-0 z-50 flex items-center justify-center overflow-x-hidden bg-neutral-800/70"
-      }
-    >
-      <div className={"fixed w-full max-w-screen-lg"} ref={modalRef}>
-        {/* Modal Box */}
-        <div className={"relative bg-gray-200 sm:rounded-lg shadow-lg"}>
-          {/* Header */}
-          <div className="absolute top-0 z-10 flex justify-center items-center w-full p-3 bg-white sm:rounded-t">
-            <h2 className="text-[12pt] font-extrabold">{title}</h2>
-
-            <span
-              onClick={handleSubmit}
-              className="cursor-pointer absolute right-4 p-1 text-blue-700 font-bold text-[10pt] border-0 hover:underline transition"
-            >
-              Done
-            </span>
-          </div>
-
-          {/* Content */}
+    <>
+      <div
+        className="
+          justify-center 
+          items-center 
+          flex 
+          overflow-x-hidden 
+          overflow-y-auto 
+          fixed 
+          inset-0 
+          z-50 
+          outline-none 
+          focus:outline-none
+          bg-neutral-800/70
+        "
+      >
+        <div
+          ref={modalRef}
+          className="
+          fixed 
+          w-full
+          max-w-screen-lg
+          my-6
+          mx-auto 
+          lg:h-auto
+          md:h-auto
+          "
+        >
+          {/*content*/}
           <div
-            style={{ paddingTop: "60px" }}
-            className="no-scrollbar w-full h-full max-h-[calc(100vh-0rem)] overflow-y-auto"
+            className={`
+            translate
+            duration-300
+            ${showModal ? "translate-y-0" : "translate-y-full"}
+            ${showModal ? "opacity-100" : "opacity-0"}
+          `}
           >
-            {body}
+            <div
+              className="
+              translate
+              h-full
+              lg:h-auto
+              md:h-auto
+              max-h-[calc(100vh-0rem)]
+              overflow-y-auto
+              border-0 
+              rounded-lg 
+              shadow-lg 
+              relative 
+              flex 
+              flex-col 
+              w-full 
+              bg-white 
+              outline-none 
+              focus:outline-none
+            "
+            >
+              {/*header*/}
+              <div
+                className="
+                flex 
+                items-center 
+                p-5
+                rounded-t
+                justify-center
+                relative
+                border-b-[1px]
+                "
+              >
+                <button
+                  className="
+                    p-1
+                    border-0 
+                    hover:opacity-70
+                    transition
+                    absolute
+                    left-9
+                  "
+                  onClick={onClose}
+                >
+                  <IoMdClose size={18} />
+                </button>
+                <span
+                  onClick={handleSubmit}
+                  className="cursor-pointer absolute right-4 p-1 text-blue-700 font-bold text-[10pt] border-0 hover:underline transition"
+                >
+                  Done
+                </span>
+                <div className="text-[12pt] font-bold">{title}</div>
+              </div>
+              {/*body*/}
+              <div className="pt-5 no-scrollbar w-full h-full max-h-[calc(100vh-0rem)] overflow-y-auto">
+                {body}
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
