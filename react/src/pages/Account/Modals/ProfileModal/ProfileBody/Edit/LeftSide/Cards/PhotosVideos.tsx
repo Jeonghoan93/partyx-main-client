cd3dcf5b98fe478efbd96a925d84cc87213a0a10
Jeonghoan Hoang <zldwkd93@gmail.com>
2023-10-13 16:39:49 +0200

    adda ll

--- react/src/pages/Account/Modals/ProfileModal/ProfileBody/Edit/LeftSide/Cards/PhotosVideos.tsx
+++ react/src/pages/Account/Modals/ProfileModal/ProfileBody/Edit/LeftSide/Cards/PhotosVideos.tsx
@@ -1,34 +1,29 @@
-import { BiChevronRight } from "react-icons/bi";
-import { BsPatchCheck } from "react-icons/bs";
 import LineContainer from "src/components/LineContainer";
+import PhotoCard from "src/components/PhotoCard";
 
 const PhotosVideos = () => {
   return (
     <LineContainer>
-      <div className="pb-5">
+      <div className="pb-3">
         <span className="text-[11pt] font-extrabold text-gray-400">
           My photos & videos
         </span>
       </div>
 
-      <div className="flex flex-col gap-4 text-gray-900 text-[11pt] font-semibold">
-        <div className="flex flex-row justify-between items-center text-center">
-          <div className="flex flex-col items-start">
-            <div className="flex flex-row gap-2 items-center hover:underline cursor-pointer">
-              <span>Selfie Verification</span>
-              <span className="bg-blue-200 border-blue-100 rounded-full">
-                <BsPatchCheck size={14} />
-              </span>
-            </div>
+      <div className="flex flex-col gap-1">
+        <div className="flex flex-row gap-1">
+          <PhotoCard />
+          <PhotoCard />
+        </div>
 
-            <span className="text-gray-400 text-[10pt]">You're verified.</span>
-          </div>
+        <div className="flex flex-row gap-1">
+          <PhotoCard />
+          <PhotoCard />
+        </div>
 
-          <span
-            onClick={() => alert("Coming soon!")}
-            className="cursor-pointer"
-          >
-            <BiChevronRight size={20} />
+        <div className="p-1">
+          <span className="text-[9pt] font-semibold text-gray-400">
+            Tap to edit; drag to recorder
           </span>
         </div>
       </div>
