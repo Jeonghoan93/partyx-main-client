17ada99df225e3032f63f5fd10f766d66f6b0095
Jeonghoan Hoang <zldwkd93@gmail.com>
2023-10-14 11:39:16 +0200

    add all to test

--- react/src/pages/Standouts/index.tsx
+++ react/src/pages/Standouts/index.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { animated, useSprings } from "react-spring";
 import { useSwipeable } from "react-swipeable";
 import { useWindowWidth } from "src/hooks/useWindowWidth";
 import { events } from "src/services/api-examples/events";
@@ -7,39 +8,55 @@ import ItemCard from "./Components/ItemCard";
 const Standouts = () => {
   const windowWidth = useWindowWidth();
   const isMobile = windowWidth < 600;
-
   const [currentCard, setCurrentCard] = useState(0);
 
-  const swipeable = useSwipeable({
-    onSwipedLeft: () => handleSwipe("left"),
-    onSwipedRight: () => handleSwipe("right"),
+  const [springs, set] = useSprings(events.length, (index) => ({
+    x: (index - currentCard) * windowWidth, // x determines the offset from the left.
+  }));
+
+  const bind = useSwipeable({
+    onSwiped: ({ dir }) => {
+      let next = currentCard + (dir === "Left" ? 1 : -1);
+      if (next < 0) next = events.length - 1; // Cyclic behavior
+      if (next >= events.length) next = 0; // Cyclic behavior
+      setCurrentCard(next);
+      set((index) => ({
+        x: (index - next) * windowWidth,
+      }));
+    },
+    onSwiping: ({ deltaX }) => {
+      set((index) => ({
+        x: (index - currentCard) * windowWidth + deltaX,
+      }));
+    },
+
     preventScrollOnSwipe: true,
     trackMouse: true,
   });
 
-  const handleSwipe = (direction: string) => {
-    if (direction === "left" && currentCard < events.length - 1) {
-      setCurrentCard(currentCard + 1);
-    } else if (direction === "right" && currentCard > 0) {
-      setCurrentCard(currentCard - 1);
-    }
-  };
   if (!isMobile) return null;
 
   return (
     <div className="fixed z-10 w-full bg-white">
-      <div className="p-3" {...swipeable}>
-        {events.map((card, index) => (
-          <ItemCard
-            key={card.eventId}
-            src={card.img}
-            title={card.title}
-            location={card.address.city}
-            attendees={card.minGuests}
-            currency={card.currency}
-            price={card.price}
-            visible={index === currentCard}
-          />
+      <div className="p-4">
+        {springs.map(({ x }, index) => (
+          <animated.div
+            key={events[index].eventId}
+            style={{
+              transform: x.to((xValue) => `translateX(${xValue}px)`),
+            }}
+            {...bind}
+          >
+            <ItemCard
+              src={events[index].img}
+              title={events[index].title}
+              location={events[index].address.city}
+              attendees={events[index].minGuests}
+              currency={events[index].currency}
+              price={events[index].price}
+              visible={index === currentCard}
+            />
+          </animated.div>
         ))}
       </div>
     </div>
